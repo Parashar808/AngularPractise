fcfa16d88a5602e19a34048db570f0701814d31a
Parashar808 <baralpara4@gmail.com>
2023-05-17 17:22:59 +0545

    Reactive Form validators

--- form/src/app/app.component.ts
+++ form/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl,FormGroup} from '@angular/forms'
+import {FormControl,FormGroup,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,20 @@ import {FormControl,FormGroup} from '@angular/forms'
 export class AppComponent {
   title = 'form';
   loginForm=new FormGroup({
-    user:new FormControl(''),
-    password:new FormControl('')
+    user:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(5)])
   }) 
 
   loginUser(){
     console.warn(this.loginForm.value);
+    // alert("Parashar Baral")
+  }
+
+  get user1(){
+    return this.loginForm.get('user')
+  }
+
+  get password1(){
+    return this.loginForm.get('password')
   }
 }
